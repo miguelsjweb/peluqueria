07b7ae40ae01f116039780f497e5cdbd65999a3a
Miguel <99533363+miguelsjweb@users.noreply.github.com>
2022-02-16 05:40:04 +0100

    Update Email.php

--- classes/Email.php
+++ classes/Email.php
@@ -3,6 +3,10 @@
 namespace Classes;
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'PHPMailer-master/src/Exception.php';
+require 'PHPMailer-master/src/PHPMailer.php';
+require 'PHPMailer-master/src/SMTP.php';
 
 class Email{
 
