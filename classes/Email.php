95a1948ec90d6aef9806339244a3ee5977bf49b0
miguelsjweb <miguelserranj@gmail.com>
2022-02-25 11:45:33 +0100

    Cambios en enlace phpmailer

--- classes/Email.php
+++ classes/Email.php
@@ -19,20 +19,20 @@ class Email{
         
         //Crear objeto de email
         $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';
+        $mail -> isSMTP();
+        $mail -> Host = 'smtp.gmail.com';
         $mail -> SMTPAuth = true ;
         $mail -> Port = 587;
         $mail -> Username = 'miguelserranj@gmail.com';
         $mail -> Password = 'rwwzxsxzyphknpyc';
 
-        $mail->setFrom('miguelserranj@gmail.com');
-        $mail->addAddress($_POST['email']);
-        $mail->Subject = 'Confirma tu cuenta';
+        $mail -> setFrom('miguelserranj@gmail.com');
+        $mail -> addAddress($_POST['email']);
+        $mail -> Subject = 'Confirma tu cuenta';
 
         //Set HTML
-        $mail->isHTML(TRUE);
-        $mail->CharSet = 'UTF-8';
+        $mail -> isHTML(TRUE);
+        $mail -> CharSet = 'UTF-8';
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->email . "</strong>. Gracias por unerte a nosotros. Para continuar, confirma tu cuenta en el siguiente enlance.</p>";
@@ -64,7 +64,7 @@ class Email{
 
         $contenido = '<html>';
         $contenido .= "<p><strong>Hola " . $this->nombre . "</strong> Has solicitado recuperar tu contraseña. Pulsa en el siguiente enlace  </p>";
-        $contenido .= "<p>Presiona aquí: <a href='http://" . $_SERVER["HTTP_HOST"] . "/recuperar?token=" . $this->token . "'>Reestablecer Contraseña</a>";
+        $contenido .= "<p>Presiona aquí: <a href='http://". $_SERVER["HTTP_HOST"] . "/confirmar-cuenta?token=".$this->token."'>Confirmar Cuenta</a> </p>";
         $contenido .= "<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje.</p>";
         $contenido .= '</html>';
         $mail->Body = $contenido;
